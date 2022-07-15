0cc486ba2445fe763f7090bfcfb6a4bbf7f6d885
yomi odusi <yomiodusi98@gmail.com>
2022-07-15 01:39:33 -0400

    web manifest complete

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,16 +4,17 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     "./index.html",
-    "./events.html",
-    "./tickets.html",
-    "./schedule.html",
-    "./assets/css/style.css",
-    "./assets/css/bootstrap.css",
-    "./assets/css/tickets.css",
-    "./dist/app.bundle.js",
-    "./dist/events.bundle.js",
-    "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
+    "./css/styles.css",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
+    "./js/idb.js",
+    "./js/index.js",
 ];
 
 self.addEventListener('install', function (e) {
