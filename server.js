165fc95d8a973e1526411fdd7f368d8a0371c7c6
yomi odusi <yomiodusi98@gmail.com>
2022-07-17 11:59:02 -0400

    updates to fix mainpage

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(express.static("public"));
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
